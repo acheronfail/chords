4c487b07e4892a2bca6c2fb752b0f5338a5785cb
acheronfail <acheronfail@gmail.com>
2025-06-01 17:16:03 +0930

    minor refactoring

--- src/lib/svelte/stores.svelte.ts
+++ src/lib/svelte/stores.svelte.ts
@@ -3,30 +3,24 @@ import { PersistentState } from "$lib/svelte/persistent-state.svelte";
 
 const C4 = 60;
 
-const SettingsSchema = z.object({
-  pianoRollMinKey: z.number(),
-  pianoRollMaxKey: z.number(),
-});
-
-type ZSettings = z.infer<typeof SettingsSchema>;
-
-const settingsDefaults = (): ZSettings => ({
-  pianoRollMinKey: C4 - 24,
-  pianoRollMaxKey: C4 + 24,
-});
-
 export const settings = new PersistentState({
   key: "settings",
-  initial: settingsDefaults,
+  initial: () => ({
+    pianoRollMinKey: C4 - 24,
+    pianoRollMaxKey: C4 + 24,
+  }),
+  schema: z.object({
+    pianoRollMinKey: z.number(),
+    pianoRollMaxKey: z.number(),
+  }),
   storageType: "localStorage",
 });
 
-interface CachedSettings {
-  midiDeviceId: string | null;
-}
-
-export const cachedSettings = new PersistentState<CachedSettings>({
+export const cachedSettings = new PersistentState({
   key: "caches",
   initial: () => ({ midiDeviceId: null }),
+  schema: z.object({
+    midiDeviceId: z.string().nullable(),
+  }),
   storageType: "localStorage",
 });
